5e13cfa3f5b800e0d4bc3f9278fcb75fae5fa62f
Bharath Kalyan S <sbharath35@gmail.com>
2021-03-25 20:09:34 +0530

    Commit

--- src/AndroidDevelopment/First.kt
+++ src/AndroidDevelopment/First.kt
@@ -1,19 +1,6 @@
 package AndroidDevelopment
 
-fun main(){
-    println("Hello World !")
-
-    val age = 21
 
 
-    println("Happy B'Day to me !!")
-    println("   ,,,,,   ")
-    println("   |||||   ")
-    println(" =========")
-    println("@@@@@@@@@@@")
-    println("{~@~@~@~@~}")
-    println("@@@@@@@@@@@")
-    println("You are ${age*365} days OLD XD ")
 
 
-}
