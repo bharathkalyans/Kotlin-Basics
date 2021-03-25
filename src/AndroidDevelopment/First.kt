c614fdde0ce4cdde3e41ede66f892726c9be6b8e
Bharath Kalyan S <sbharath35@gmail.com>
2021-03-25 19:24:42 +0530

    BirthDay Card

---
+++ src/AndroidDevelopment/First.kt
@@ -0,0 +1,19 @@
+package AndroidDevelopment
+
+fun main(){
+    println("Hello World !")
+
+    val age = 21
+
+
+    println("Happy B'Day to me !!")
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    println("You are ${age*365} days OLD XD ")
+
+
+}
