f114f56e8b4f9a68761fe6e65771c2465f6d7f61
Bharath Kalyan S <sbharath35@gmail.com>
2021-03-26 15:03:53 +0530

    Commit

--- src/AndroidDevelopment/First.kt
+++ src/AndroidDevelopment/First.kt
@@ -1,6 +1,28 @@
 package AndroidDevelopment
 
+import java.util.*;
+import kotlin.random.Random
 
+
+fun main(args : Array<String>){
+
+    val name:String = "Bharath"
+    val p = print(name)
+
+    println(p)
+
+    val temp =20
+    val message = "Is Hot ${temp > 23} "
+    print(randomDay())
+
+}
+
+
+fun randomDay():String{
+    val days : List<String> = listOf("Monday","Tuesday","Wednesday","Thursday")
+
+    return days[Random.nextInt(4)]
+}
 
 
 
