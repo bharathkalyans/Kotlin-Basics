8df3993a008e6465393084c88b95138b250c199e
Bharath Kalyan S <sbharath35@gmail.com>
2021-03-29 21:38:29 +0530

    For and Until

--- src/TeluskoTutorials/Aliens.kt
+++ src/TeluskoTutorials/Aliens.kt
@@ -10,4 +10,11 @@ class Aliens {
 }
 fun main(args : Array<String>){
     println(Int.MIN_VALUE)
+
+    var rangeIs = 100.downTo(1)
+
+    for (i in rangeIs step 10) println(i)
+
+    for (i in 1 until 19) print("$i  ")//until will not include last value.
+
 }
