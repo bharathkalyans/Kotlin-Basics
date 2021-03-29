554374814e6148d08fd992c8fc8f433b8c13c0de
Bharath Kalyan S <sbharath35@gmail.com>
2021-03-29 20:17:20 +0530

    Operators

---
+++ src/TeluskoTutorials/Operators.kt
@@ -0,0 +1,11 @@
+package TeluskoTutorials
+
+fun main(){
+
+    var numbers : Int = 2
+    var number2 : Int = 23
+    println(numbers.plus(number2).minus(90).div(12).toFloat())
+
+    println(number2.plus(numbers).plus(222))
+
+}
