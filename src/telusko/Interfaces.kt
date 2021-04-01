5a1ceddcdc97fb78dd84f861b0f3cec1f69fcf7d
Bharath Kalyan S <sbharath35@gmail.com>
2021-04-01 17:14:44 +0530

    Interfaces modified !

--- src/telusko/Interfaces.kt
+++ src/telusko/Interfaces.kt
@@ -7,17 +7,30 @@ interface A{
         //Implement a function so in future it doesn't crash Classes
         //implementing this Interface.
     }
+    fun abc(){
+        println("In A ")
+
+    }
 }
 
 interface B{
     fun swim()
     fun think()
+    fun abc(){
+        println("In B ")
+
+    }
 }
 
 
 class C : A,B{
     override fun think() {
     println("Thinking Nigga")
+    }
+
+    override fun abc() {
+        println("In C ")
+        super<A>.abc()
     }
 
     override fun swim() {
@@ -28,4 +41,6 @@ class C : A,B{
 
 fun main(args: Array<String>){
 
+    var obj =  C()
+    obj.abc()
 }
